8a67a7df909a2731272f03f1269c91bc635381d7
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-07-24 22:38:39 +0300

    added - Services page

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,36 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  document.getElementsByTagName('body')[0].classList.add('loaded');
-});
+
+function scrollWinToPreliminaryStudies() {
+  var d = document.getElementById("preliminaryStudies");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
+
+function scrollWinToDesignWorks() {
+  var d = document.getElementById("designWorks");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
+
+function projectManagement() {
+  var d = document.getElementById("projectManagement");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
+
+function scrollWinToInstallationSupervision() {
+  var d = document.getElementById("installationSupervision");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
+
+function scrollWinToCommissioning() {
+  var d = document.getElementById("commissioning");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
+
+function scrollWinToEquipmentMaterialSupplies() {
+  var d = document.getElementById("equipmentMaterialSupplies");
+  let topPos = parseInt(d.offsetTop) - 80;
+  window.scrollTo(0, topPos);
+}
